04e8f157bf85d439e90b15c09e6f59be3eb0ac99
jameson141 <jamesonmiyashiro@gmail.com>
2018-02-08 18:58:52 -1000

    first commit

--- js/pizza.js
+++ js/pizza.js
@@ -4,6 +4,50 @@
 * Each object will have two nested objects ex.(pizza & toppings)
 * Keys with more than one value should be an array of strings ex. Veggies
 */
+var order11821 = {
+	firstname:"James",
+	lastname:"Franko",
+	address:"2155 E Lake Dr",
+	city:"Seattle",
+	zipcode:11310,
+	pizza: {
+		crust:"thin",
+		pizzasauce:"robust marinara",
+		cheese: "regular",
+		toppings: {
+			veggies: "olives", "green peppers",
+			meats: "pepperoni", "ham", "sasusage",
+
+			
+		}
+	},
+	sideitems:"breadsticks", "bbq wings 6 piece"
+	Sauces:"ranch",
+	Specialinstructions:"please cut into squares",
+	paymenttype:"cash",
+	storeId:112
+
+}
+
+var order11372 = {
+	firstname:"Justin",
+	lastname:"Bieber",
+	address:"1801 W Rodeo Dr",
+	city:"Beverly Hills",
+	state:"CA",
+	zipcode:91833,
+	pizza: {
+		crust:"stuffed crust",
+		pizzasauce:"creamy garlic alfredo",
+		cheese:"double",
+		toppings: {
+			veggies:""
+		}
+	}
+}
+
+		
+
 
 
 /*
